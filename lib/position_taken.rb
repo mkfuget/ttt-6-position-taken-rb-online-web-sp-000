93df9ffe9d13b076880209339d1b0e17eb753fbe
Michael Fuget <michaelfuget@gmail.com>
2020-12-01 00:43:28 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if(index<0 || index>8)
+    return false 
+  end
+  entry = board[index]
+  if(entry == " " || entry == "" || entry == nil)
+    return false 
+  end
+  return true
+end
